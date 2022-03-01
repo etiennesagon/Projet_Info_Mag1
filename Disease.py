0ef031735c281b3588974a026043533eaa85d8ec
etiennesagon <94171554+etiennesagon@users.noreply.github.com>
2022-03-01 09:24:33 +0100

    Update Disease.py
    J'ai crée la classe Disease + un exemple de maladie pour voir si ça marche

--- Disease.py
+++ Disease.py
@@ -1,4 +1,12 @@
+from PyQt5.QtGui import QColor
 
-class Disease():
-    def __init__(self):
-        self.etst = 0
+class Disease() :
+    def __init__(self, color, virulence, duration):
+        self.color = color
+        self.virulence = virulence
+        self.duration = duration
+
+#test de maladie pour voir si ça marche
+couleur1 = QColor.fromRgbF(0.5, 0.5, 0.5, 1)
+maladie = Disease(couleur1, 1, 30)
+
